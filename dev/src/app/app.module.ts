efff1510bd0be3938f5a217d6912900ce14e5e60
Daniel Culotta <dannykoolaid@Daniels-MacBook-Air.local>
2018-08-21 11:35:53 -0600

    √Creating Fire

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { Part2Component } from './components/part2/part2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    Part2Component
   ],
   imports: [
     BrowserModule
