bbff264ab6860857282ea8cc80d2260137776ba6
Daniel Culotta <dannykoolaid@Daniels-MacBook-Air.local>
2018-08-22 22:07:36 -0600

    😎First javascript complete

--- dev/src/app/app.module.ts
+++ dev/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,6 +10,22 @@ import { Part2Component } from './components/part2/part2.component';
 import { OutletsComponent } from './components/outlets/outlets.component';
 import { RecentPhotosComponent } from './components/recent-photos/recent-photos.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { TeamPageComponent } from './pages/team-page/team-page.component';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'portfolio', component: PortfolioPageComponent },
+  { path: 'team', component: TeamPageComponent },
+  { path: 'contact', component: ContactPagesComponent },
+  { path: '**', component: HomeComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -19,10 +36,16 @@ import { FooterComponent } from './components/footer/footer.component';
     Part2Component,
     OutletsComponent,
     RecentPhotosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutPageComponent,
+    PortfolioPageComponent,
+    TeamPageComponent,
+    ContactPagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
